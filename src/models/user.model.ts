4a896e0e26c6fd1e911c10dc8cadfe63b1b5567a
Lior Vainer <liorvainer@gmail.com>
2025-01-08 15:08:35 +0200

    add authentication routes, user model; update TypeScript configuration and package scripts

---
+++ src/models/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose, {Document} from "mongoose";
+
+
+export interface User {
+    email: string;
+    password: string;
+    _id?: string;
+    refreshTokens?: string[];
+}
+
+const userSchema = new mongoose.Schema<User>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    refreshTokens: {
+        type: [String],
+        default: [],
+    }
+});
+
+export const userModel = mongoose.model<User>("Users", userSchema);
+export type UserDocument = Document<unknown, {}, User> & User & Required<{
+    _id: string;
+}> & {
+    __v: number;
+}
